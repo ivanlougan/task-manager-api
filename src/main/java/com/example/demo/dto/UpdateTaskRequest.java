15721fc928c37011d7bc6c8ad4029c2ed7f8adb3
Robert Zdeb <robinio123@gmail.com>
2026-05-21 22:25:18 +0100

    deleted setters in UpdateTaskRequest class

--- src/main/java/com/example/demo/dto/UpdateTaskRequest.java
+++ src/main/java/com/example/demo/dto/UpdateTaskRequest.java
@@ -20,15 +20,8 @@ public class UpdateTaskRequest {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public boolean isCompleted() {
         return completed;
     }
 
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
 }
