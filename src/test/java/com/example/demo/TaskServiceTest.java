8f18f2b11bea242b196c1537ec889d80b903f2a1
Robert Zdeb <robinio123@gmail.com>
2026-05-22 22:42:05 +0100

    added shouldCreateTask test and updated shouldUpdateTask test

--- src/test/java/com/example/demo/TaskServiceTest.java
+++ src/test/java/com/example/demo/TaskServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.dto.CreateTaskRequest;
 import com.example.demo.dto.TaskResponse;
 import com.example.demo.dto.UpdateTaskRequest;
 import com.example.demo.model.Task;
@@ -7,18 +8,15 @@ import com.example.demo.repository.TaskRepository;
 import com.example.demo.service.TaskService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
 
@@ -28,31 +26,56 @@ public class TaskServiceTest {
     @InjectMocks
     private TaskService taskService;
 
+
+    @Test
+    void shouldCreateTask() {
+        CreateTaskRequest request = new CreateTaskRequest("Learn Python cause AI..");
+
+        Task task = new Task(
+                "Learn Python cause AI..",
+                false
+        );
+
+        task.setId(1L);
+
+        when(taskRepository.save(any(Task.class)))
+                .thenReturn(task);
+
+        TaskResponse response = taskService.createTask(request);
+
+        assertEquals(1L, response.id());
+        assertEquals("Learn Python cause AI..", response.title());
+        assertFalse(response.completed());
+
+        verify(taskRepository).save(any(Task.class));
+    }
+
     @Test
     void shouldUpdateTask() {
 
-        Task existingTask = new Task();
+        Task task = new Task(
+                "Old title",
+                false
+        );
 
-        existingTask.setId(1L);
-        existingTask.setTitle("Old title");
-        existingTask.setCompleted(false);
+        task.setId(1L);
 
-        UpdateTaskRequest request = new UpdateTaskRequest();
-
-        ReflectionTestUtils.setField(request, "title", "New title");
-        ReflectionTestUtils.setField(request, "completed", true);
+        UpdateTaskRequest request =
+                new UpdateTaskRequest(
+                        "New title",
+                        true
+                );
 
         when(taskRepository.findById(1L))
-                .thenReturn(Optional.of(existingTask));
+                .thenReturn(Optional.of(task));
 
         when(taskRepository.save(any(Task.class)))
-                .thenReturn(existingTask);
+                .thenReturn(task);
 
         TaskResponse response = taskService.updateTask(1L, request);
 
-        assertEquals("New title", response.getTitle());
-
-        assertTrue(response.isCompleted());
+        assertEquals("New title", response.title());
+        assertTrue(response.completed());
 
     }
 }
